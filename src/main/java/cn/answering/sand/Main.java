967b819fdd18d11c7cf603f2165c0546a1a58e65
zhangjp <jianping.zhang2@amh-group.com>
2022-05-12 13:51:16 +0800

    init sand

---
+++ src/main/java/cn/answering/sand/Main.java
@@ -0,0 +1,13 @@
+package cn.answering.sand;
+
+/**
+ * @author zhangjp2
+ * @date 2022/5/12 1:50 PM
+ * @description
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("this is sand");
+    }
+}
